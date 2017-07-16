6a32957cfb46c9fdc062dc9474bde132f9468d77
WizardLink <alexandre.cssilva@gmail.com>
2017-07-16 17:21:19 -0300

    Forgot about this sorry.

--- cogs/client/event_handler.js
+++ cogs/client/event_handler.js
@@ -5,7 +5,7 @@ const { client } = require('../connections/discord');
 
 exports.start = () =>
 {	readdir('./cogs/client/events/', (err, files) =>
-	{	if(err) console.error(err);
+	{	if(err) return console.error(err);
 		log(`Loading ${files.length} Client Events`);
 		files.forEach(f =>
 		{	client.on(f, (...args) => event(f)(client, ...args));	});	});	}
