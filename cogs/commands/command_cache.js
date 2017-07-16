bac79c3a61bb52b1afab9d620af746349712fe1c
WizardLink <alexandre.cssilva@gmail.com>
2017-07-16 17:21:19 -0300

    Forgot about this sorry.

--- cogs/commands/command_cache.js
+++ cogs/commands/command_cache.js
@@ -3,14 +3,14 @@ const { client } = require('../connections/discord');
 const log = require('../../util/log/load');
 
 exports.start = () =>
-{	readdir('./commands/'), (err, files) =>
-	{	if(err) console.error(err);
+{	readdir('./commands/', (err, files) =>
+	{	if(err) return console.error(err);
 		files.forEach(f =>
-		readdir(`./commands/${f}/`, (err, files) =>
-		{	if(err) console.error(err);
-			f.forEach(c =>
-			{	let commandFile = require(`../commands/${f}/${c}`);
-				//Here is where we define the command
-				client.commands.set(cmd.name, cmd);
-				cmd.alias.forEach(alias =>
-				{	client.aliases.set(alias, cmd.name);	});	});	}));	}	}
+		{	readdir(`./commands/${f}`, (err, files) =>
+			{	if(err) return console.error(err);
+				f.forEach(c =>
+				{	let commandFile = require(`../../commands/${f}/${c}`);
+					//Here is where we define the cmd
+					client.commands.set(cmd.name, cmd);
+					cmd.alias.forEach(alias =>
+					{	client.aliases.set(alias, cmd.name);	});	});	});	});	});	}
