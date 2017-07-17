cea5e1335aaa6d7ccf1de56c6e389c48c331bfe3
SpaceEEC <wermail2002-kap@yahoo.de>
2017-07-17 02:52:36 +0200

    util/client changes: - Fixed instanceof error - Used instanceof whever possible - Fixed space indent in fetch.js - Changed order of validating calls to account for the incoming order via parameters - Also left two commands about things that might error in the future ~~- I don't really like the indent style, even though that's on you~~

--- util/client/error/fetch.js
+++ util/client/error/fetch.js
@@ -1,10 +1,11 @@
+const { Message } = require('discord.js');
+
 module.exports = (user, message) =>
 {
-    if(!user || !message) // Check if the parameters exist
-    {   throw new Error('Fetch error takes two parameters, User and Message');  }
-    else
-    {   if(typeof message !== 'object') throw new Error('Message must be an Object');
-        if(typeof user !== 'string') throw new Error('User must be a String');  }
-                // Check if parameters are valid
-    
-    message.channel.send(`I couldn't find anything matching **${user}** <:ayy:315270615844126720>`);    } // Pretty obvious am i rite
+	if(!user || !message) // Check if the parameters exist
+	{	throw new Error('Fetch error takes two parameters, User and Message');  }
+	else
+	{	if(!(message instanceof Message)) throw new Error('message parameter isn\'t instanceof Message!');
+		if(typeof user !== 'string') throw new Error('User must be a string');  }
+
+	message.channel.send(`I couldn't find anything matching **${user}** <:ayy:315270615844126720>`);	} // Pretty obvious am i rite
