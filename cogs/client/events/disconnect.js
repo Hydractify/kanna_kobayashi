2a70bf110b213ceb06990dd8700ba28992a372a4
Anxeal <anxealous@gmail.com>
2017-07-18 11:36:21 +0300

    Add missing parameter "disconnect"

--- cogs/client/events/disconnect.js
+++ cogs/client/events/disconnect.js
@@ -1,5 +1,5 @@
 const log = require('../../../util/log/bot');
 const moment = require('moment');
 
-module.exports = (client) =>
+module.exports = async (client, disconnect) =>
 {	return log(`Bot disconnected ${moment().format('HH:mm \\[DD/MM/YYYY\\]')}\nCode: ${disconnect.code}\nReason: ${disconnect.reason}`);	}
