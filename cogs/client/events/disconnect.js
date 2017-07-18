d2690c86b292e170aebfefe3fb2a2da1118e3e6b
Anxeal <anxealous@gmail.com>
2017-07-18 11:32:24 +0300

    Make moment formatting look neater

--- cogs/client/events/disconnect.js
+++ cogs/client/events/disconnect.js
@@ -2,4 +2,4 @@ const log = require('../../../util/log/bot');
 const moment = require('moment');
 
 module.exports = (client) =>
-{	return log(`Bot disconnected ${moment().format('HH:mm')} [${moment().format('DD/MM/YYYY')}]\nCode: ${disconnect.code}\nReason: ${disconnect.reason}`);	}
+{	return log(`Bot disconnected ${moment().format('HH:mm \\[DD/MM/YYYY\\]')}\nCode: ${disconnect.code}\nReason: ${disconnect.reason}`);	}
--- cogs/client/events/reconnecting.js
+++ cogs/client/events/reconnecting.js
@@ -2,4 +2,4 @@ const moment = require('moment');
 const log = require('../../../util/log/bot');
 
 module.exports = async (client) =>
-{	log(`Bot reconnecting ${moment().format('HH:mm')} [${moment().format('DD/MM/YYYY')}]`);	}
+{	log(`Bot reconnecting ${moment().format('HH:mm \\[DD/MM/YYYY\\]')}`);	}
