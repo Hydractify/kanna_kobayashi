cfc09081a90f5fe22e745888ee832e49d78586ba
WizardLink <alexandre.cssilva@gmail.com>
2017-07-18 23:15:53 -0300

    Sorry, cannot finish Commands today
    - Fixed fetching user
    - Load all commands correctly
    - Added 4 Commands
    
    Cannot finish due to family problems

--- cogs/client/events/ready.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client) =>
-{	if(!client.user.presence.game)
-	{	const a = await client.shard.fetchClientValues('guilds.size');
-		const b = a.reduce((p, n) => {	return p + n, 0});
-		client.user.setGame(`k!help | on ${b} guilds`);	}	}
