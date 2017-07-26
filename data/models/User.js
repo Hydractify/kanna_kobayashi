e01255e849e69f1f0c03c5ace807515d41225c6e
Anxeal <anxealous@gmail.com>
2017-07-26 21:41:04 +0300

    Fix models

--- data/models/User.js
+++ data/models/User.js
@@ -2,11 +2,13 @@ const thinky = require('../../util/data/thinky');
 const type = thinky.type;
 
 const User = thinky.createModel('User', {
-    id: type.number().integer(),
-    coins: type.number().integer(),
-    experience: type.number().integer()
+    id: type.string(),
+    coins: type.number().integer().default(0),
+    exp: type.number().integer().default(0)
 });
 
-User.define('getLevel', () => Math.floor(Math.sqrt(this.experience / 100)) + 1);
+User.define('getLevel', function() {
+    return Math.floor(Math.sqrt(this.exp / 1000)) + 1
+});
 
 module.exports = User;
