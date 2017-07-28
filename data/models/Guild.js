8a274fe32ca268bc0f3443a96273756d708a770e
Anxeal <anxealous@gmail.com>
2017-07-28 18:59:21 +0300

    Add default values to objects in the Guild model

--- data/models/Guild.js
+++ data/models/Guild.js
@@ -7,15 +7,15 @@ const Guild = thinky.createModel('Guild', {
     roles: type.object().schema({
         mod: type.string().default('Human Tamer'),
         game: type.string().default('Dragon Tamer')
-    }),
+    }).default({}),
     quiz: type.object().schema({
         character: type.string().default('http://pm1.narvii.com/6366/2c35594538206f7f598be792bf203b6b638e9c07_hq.jpg'),
         answer: type.string().default('Kanna Kobayashi')
-    }),
+    }).default({}),
     notifications: type.object().schema({
         levelUp: type.boolean().default(true),
         welcome: type.boolean().default(false)
-    })
+    }).default({})
 })
 
 module.exports = Guild;
