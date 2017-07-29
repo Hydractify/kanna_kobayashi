43a4145af8b7d213cfc29abe5951bab27669c919
Anxeal <anxealous@gmail.com>
2017-07-29 07:51:05 +0300

    Fix client

--- cogs/connections/discord.js
+++ cogs/connections/discord.js
@@ -5,7 +5,7 @@ const beta = require('../../data/client/beta');
 
 module.exports = class Discord_JS
 {	static start()
-	{	const Client = new Discord.Client();
+	{	const Client = new Discord.Client({ disableEveryone : true });
 		Client.login(beta.token);
 		log('Connected to Discord!');
 		Client.on('ready', async () =>
@@ -14,7 +14,6 @@ module.exports = class Discord_JS
 				const b = a.reduce((p, n) => {	return p + n, 0});
 				client.user.setGame(`k!help | on ${b} guilds`);	}*/
 			log(`Connected as ${Client.user.tag} (${Client.user.id}) at ${require('moment')().format('HH:mm \\[DD/MM/YYYY\\]')}`);
-			require('./rethinkdb').start();
 			require('./apis/dbl').start();
 			require('./apis/dbots').start();;
 			require('../client/perm_level').start();
