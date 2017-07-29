699107ad6f0afd45fbd8dc7b34617ae26c556ede
Anxeal <anxealous@gmail.com>
2017-07-29 07:51:28 +0300

    Make event handler wait for dbReady

--- cogs/client/event_handler.js
+++ cogs/client/event_handler.js
@@ -1,14 +1,23 @@
-const event = (event) => {	return require(`./events/${event}`);	};
+const event = (event) => { return require(`./events/${event}`); };
 const { readdir } = require('fs');
 const log = require('../../util/log/load');
 const log1 = require('../../util/log/error');
 const { client } = require('../connections/discord');
+const thinky = require('../../util/data/thinky');
+const databaseLog = require('../../util/log/database');
 
-exports.start = () =>
-{	readdir('./cogs/client/events/', (err, files) =>
-	{	if(err) return log1(err.stack, err)
-		files.forEach(f =>
-		{	const eName = f.replace('.js', '');
-			client.on(eName, (...args) => event(eName)(client, ...args)
-			.catch(err => log1(`Unexpected error at event ${eName}\n\n${err.stack}`, err)));	});	
-		log(`Loaded ${files.length} client Events`);	});	}
+
+exports.start = () => {
+    thinky.dbReady().then(() => {
+        databaseLog('Thinky connected to RethinkDB!');
+        readdir('./cogs/client/events/', (err, files) => {
+            if (err) return log1(err.stack, err)
+            files.forEach(f => {
+                const eName = f.replace('.js', '');
+                client.on(eName, (...args) => event(eName)(client, ...args)
+                    .catch(err => log1(`Unexpected error at event ${eName}\n\n${err.stack}`, err)));
+            });
+            log(`Loaded ${files.length} client Events`);
+        });
+    })
+}
