4b79c94e233878a0cf3d5cb27d19cddaa5e81cc8
SpaceEEC <wermail2002-kap@yahoo.de>
2017-07-29 02:32:26 +0200

    Cleanup: cogs/client/events/message.js - fixed prefix parsing/slicing (hopefully) - converted indent to 4 tabs - renamed snek_case variables to camelCase ones - renamed g to guild - moved inline comments to second line - removed some obvious comments // it's fine, really - reworded permission error message

--- cogs/client/events/message.js
+++ cogs/client/events/message.js
@@ -1,72 +1,80 @@
-const embed_color = require('../../../util/client/embed_color');
-const error_message = require('../../../util/client/error/stack');
+const embedColor = require('../../../util/client/embed_color');
+const errorMessage = require('../../../util/client/error/stack');
 const blackFile = require('../../../data/client/blacklist');
 const info = require('../../../data/client/info.json');
 const { CommandLog, Guild, User } = require('../../../data/Models');
 
-module.exports = async(client, message) => {
-    if (blackFile.includes(message.guild.owner.user.id)) return;
-    if (blackFile.includes(message.author.id)) return;
+module.exports = async (client, message) => {
+	if (blackFile.includes(message.guild.owner.user.id)) return;
+	if (blackFile.includes(message.author.id)) return;
 
-    let g;
-    try {
-        g = await Guild.get(message.guild.id).run();
-    } catch (err) {
-        g = new Guild({ id: message.guild.id });
-        g.save();
-    }
+	let guild;
+	try {
+		guild = await Guild.get(message.guild.id).run();
+	} catch (err) {
+		guild = new Guild({ id: message.guild.id });
+		guild.save();
+	}
 
-    let prefixes = info.defaultPrefixes.slice(0);
-    prefixes.push(g.prefix.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')); // Escaping regex
-    let regex = new RegExp(`^(${prefixes.join('|')})([^]*)`);
-    let match = message.content.match(regex);
+	let prefixes = info.defaultPrefixes.slice(0);
+	// super special regex chars escape regex
+	prefixes.push(guild.prefix.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&'));
+	let regex = new RegExp(`^(${prefixes.join('|')})([^]*)`);
 
-    if (!match) return;
+	if (!regex.test(message.content)) return;
 
-    let [command, ...args] = match[2].split(' ');
-    let cmd = client.commands.get(command);
-    let perm = client.perms(message);
-    let color = embed_color(message);
+	let prefixLength = regex.exec(message.content)[0].length;
 
-    if (!cmd) {
-        let alias = client.aliases.get(command);
-        if (alias) {
-            cmd = client.commands.get(alias);
-            command = alias;
-        }
-    }
-    if (!cmd) return; // Command doesn't exist
-    if (perm < cmd.permLevel) return message.channel.send(`${message.author} you don't have permission to use that command!`); // Not enough permission
+	let [command, ...args] = message.content.slice(prefixLength).split(' ');
+	let cmd = client.commands.get(command);
+	let perm = client.perms(message);
+	let color = embedColor(message);
 
-    let log;
-    try {
-        log = await CommandLog.filter({ userId: message.author.id, command }).nth(0).run();
-    } catch (err) {
-        log = new CommandLog({ userId: message.author.id, command });
-    }
+	if (!cmd) {
+		let alias = client.aliases.get(command);
+		if (alias) {
+			cmd = client.commands.get(alias);
+			command = alias;
+		} else {
+			return;
+		}
+	}
 
-    let time = new Date();
+	if (perm < cmd.permLevel) {
+		message.channel.send(`${message.author}, you don't have the required permission level to use this command!`);
+		return;
+	}
 
-    if (log.lastUsed === null || log.lastUsed.getTime() + cmd.cooldown <= time.getTime()) {
-        log.lastUsed = new Date();
-    } else {
-        return message.reply(`**${cmd.name}** is on cooldown!`);
-    }
-    log.save();
+	let log;
+	try {
+		log = await CommandLog.filter({ userId: message.author.id, command }).nth(0).run();
+	} catch (err) {
+		log = new CommandLog({ userId: message.author.id, command });
+	}
 
-    let user;
-    try {
-        user = await User.get(message.author.id).run();
-    } catch (err) {
-        user = new User({ id: message.author.id });
-    }
-    user.exp += cmd.exp;
-    user.coins += cmd.coins;
-    user.save();
+	let time = new Date();
+	if (log.lastUsed === null
+		|| log.lastUsed.getTime() + cmd.cooldown <= time.getTime()) {
+		log.lastUsed = new Date();
+	} else {
+		message.reply(`**${cmd.name}** is on cooldown!`);
+		return;
+	}
+	log.save();
 
-    try {
-        await cmd.run(message, color, args, perm);
-    } catch (err) {
-        error_message(err, message, cmd);
-    }
-}
+	let user;
+	try {
+		user = await User.get(message.author.id).run();
+	} catch (err) {
+		user = new User({ id: message.author.id });
+	}
+	user.exp += cmd.exp;
+	user.coins += cmd.coins;
+	user.save();
+
+	try {
+		await cmd.run(message, color, args, perm);
+	} catch (err) {
+		errorMessage(err, message, cmd);
+	}
+};
