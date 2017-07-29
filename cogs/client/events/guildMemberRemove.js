2917f8a2ac9bee3a3adbd58608955537053aed58
Anxeal <anxealous@gmail.com>
2017-07-29 06:55:18 +0300

    Add ability to actually disable welcome messages

--- cogs/client/events/guildMemberRemove.js
+++ cogs/client/events/guildMemberRemove.js
@@ -1,2 +1,12 @@
-module.exports = async (client, member) =>
-{	await member.guild.defaultChannel.send(`Sad to see you depart **${member.user.tag}**... <:ayy:315270615844126720>`);	}
+module.exports = async(client, member) => {
+    if (!member.guild.defaultChannel.permissionsFor(member.guild.me).has('SEND_MESSAGES')) return;
+    let guild;
+    try {
+        guild = await Guild.get(member.guild.id).run();
+    } catch (err) {
+        guild = new Guild({ id: member.guild.id });
+        guild.save();
+    }
+    if (!guild.notifications.welcome) return;
+    await member.guild.defaultChannel.send(`Sad to see you depart **${member.user.tag}**... <:ayy:315270615844126720>`);
+}
