813747b9eeca63142eae6510f6eb832d0cf9e0a2
SpaceEEC <wermail2002-kap@yahoo.de>
2017-09-28 00:15:30 +0200

    add Raven#config and install temporarily to index file
    Creating a whole file for a single line of code feels unnecessary, might need a better place for that single line however.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { Guild, GuildMember } = require('discord.js');
+const Raven = require('raven');
 
 const GuildExtension = require('./extensions/Guild');
 const GuildMemberExtension = require('./extensions/GuildMember');
 const Client = require('./structures/Client');
+
+Raven.config('https://27e26b64bed14c6a84bd91f35a52d914:6f98cea9e01a4eba86cfe139ed97e8aa@sentry.io/196287').install();
 
 GuildExtension.extend(Guild);
 GuildMemberExtension.extend(GuildMember);
