ca14852967070ea85f6c10033edef91dd4166816
SpaceEEC <wermail2002-kap@yahoo.de>
2017-09-28 18:40:41 +0200

    fix/feature(index): add handler for unhandled rejections, connect to thte database, and login the client

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const Raven = require('raven');
 const GuildExtension = require('./extensions/Guild');
 const GuildMemberExtension = require('./extensions/GuildMember');
 const Client = require('./structures/Client');
+const Database = require('./structures/PostgreSQL');
+const Logger = require('./structures/Logger');
 
+process.on('unhandledRejection', Logger.instance.error.bind(Logger.instance, '[REJECTION]:'));
 Raven.config('https://27e26b64bed14c6a84bd91f35a52d914:6f98cea9e01a4eba86cfe139ed97e8aa@sentry.io/196287').install();
+
+Database.instance.start();
 
 GuildExtension.extend(Guild);
 GuildMemberExtension.extend(GuildMember);
 
 const client = new Client({ disableEveryone: true });
+
+client.login();
