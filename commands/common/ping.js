0e386832f9be490a0d9c2724702bb0a3d115b94c
SpaceEEC <wermail2002-kap@yahoo.de>
2017-10-04 14:07:29 +0200

    grammar(PingCommand): fix grammar

--- commands/common/ping.js
+++ commands/common/ping.js
@@ -7,7 +7,7 @@ class PingCommand extends Command {
 			coins: 0,
 			exp: 0,
 			usage: 'ping',
-			description: 'See how much time i take to receive your message!',
+			description: 'See how much time it takes me to receive your message!',
 			name: 'ping',
 			permLevel: 0,
 			examples: ['ping']
@@ -15,9 +15,9 @@ class PingCommand extends Command {
 	}
 
 	async run(message) {
-		const Message = await message.channel.send('Searching for insects... <:KannaISee:315264557843218432>');
-		await Message.edit([
-			`I took **${Message.createdTimestamp - message.createdTimestamp}ms** to find and eat all insects!`,
+		const sent = await message.channel.send('Searching for insects... <:KannaISee:315264557843218432>');
+		await sent.edit([
+			`It took me **${sent.createdTimestamp - message.createdTimestamp}ms** to find and eat all insects!`,
 			` <:KannaOh:315264555859181568> ~~**(WS: ${Math.floor(this.client.ping)}ms)**~~`
 		]);
 	}
