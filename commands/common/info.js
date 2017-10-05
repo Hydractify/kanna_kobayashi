c22df3672afa594b28d5b997a7e40208589000b4
SpaceEEC <wermail2002-kap@yahoo.de>
2017-10-05 15:47:10 +0200

    feature(InfoCommand): add info command

---
+++ commands/common/info.js
@@ -0,0 +1,32 @@
+const Command = require('../../structures/Command');
+const RichEmbed = require('../../structures/RichEmbed');
+
+class InfoCommand extends Command {
+	constructor(handler) {
+		super(handler, {
+			aliases: ['invite', 'patreon', 'guild', 'ghearts'],
+			clientPermissions: ['EMBED_LINKS'],
+			coins: 0,
+			description: 'All the useful links you need!',
+			examples: ['info'],
+			exp: 0,
+			name: 'info',
+			usage: 'info'
+		});
+	}
+
+	run(message) {
+		const embed = RichEmbed.common(message)
+			.setAuthor(`${this.client.user.username} Info`, this.client.user.displayAvatarURL)
+			.setDescription('\u200b')
+			.addField('Invite', 'http://kannathebot.me/invite', true)
+			.addField('Patreon', 'http://kannathebot.me/patreon', true)
+			.addField('Official Guild', 'https://discord.gg/uBdXdE9', true)
+			.addField('Official Website', 'http://kannathebot.me', true)
+			.setThumbnail(this.client.user.displayAvatarURL);
+
+		return message.channel.send(embed);
+	}
+}
+
+module.exports = InfoCommand;
