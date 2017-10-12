474f2743566e24f91051aafb262941ec6a4e2ff2
SpaceEEC <wermail2002-kap@yahoo.de>
2017-10-12 11:47:23 +0200

    feat/refactor(Donate): new alias, better validating, fix typos and add error rollback
    - New alias: 'trade'
    This is a common alias or even command name to transfer money from your account to another, only makes sense to have.
    - Remove unused Item and User model require
    - Resolve and validate arguments in the order they are expected, makes more sense imo.
    - Disallow negative amounts to be donated, users could 'steal' money that way from everyone.
    - Wrap the transfer in a transaction to rollback both if one errors.
    - Fixed a few typos à la 'transfered' -> 'transferred' and 'an' -> 'a'.

--- commands/user/donate.js
+++ commands/user/donate.js
@@ -1,11 +1,10 @@
-const Item = require('../../models/Item');
-const User = require('../../models/User');
 const Command = require('../../structures/Command');
+const { instance: { db } } = require('../../structures/PostgreSQL');
 
 class DonateCommand extends Command {
 	constructor(handler) {
 		super(handler, {
-			aliases: ['give', 'transfer'],
+			aliases: ['give', 'transfer', 'trade'],
 			coins: 0,
 			description: 'Give money to someone!',
 			examples: ['give wizard 1000'],
@@ -17,24 +16,32 @@ class DonateCommand extends Command {
 	}
 
 	async run(message, [input, amount]) {
-		if (!input) return message.reply(`you must give me an user! (\`${this.usage}\`)`);
+		if (!input) return message.reply(`you must give me a user! (\`${this.usage}\`)`);
 		if (!amount) return message.reply(`you must give me an amount! (\`${this.usage}\`)`);
-		
-		const userAmount = parseInt(amount);
-		if (isNaN(userAmount)) return message.reply('you must give me a valid number!');
-
-		const authorModel = message.author.model || await message.author.fetchModel();
-		if (authorModel.coins < userAmount) return message.reply('you don\'t have that amount of money to donate!');
 
 		const mentionedUser = await this.handler.resolveUser(input, false);
+		if (!mentionedUser) return message.reply(`could not find a non bot user with ${input}.`);
 		const mentionedModel = mentionedUser.model || await mentionedUser.fetchModel();
+
+		const userAmount = parseInt(amount);
+		if (isNaN(userAmount) || userAmount <= 0) {
+			return message.reply('you must give me a valid amount, which must be a positive number!');
+		}
+
+		const authorModel = message.author.model || await message.author.fetchModel();
+		if (authorModel.coins < userAmount) return message.reply('you do not have that amount of money to donate!');
+
+		const transaction = await db.transaction();
 
 		mentionedModel.coins += amount;
 		authorModel.coins -= amount;
-		await authorModel.save();
-		await mentionedModel.save();
 
-		return message.channel.send(`Sucessfully transfered **${amount}** <:coin:330926092703498240> to ${mentionedUser}!`);
+		await authorModel.save({ transaction });
+		await mentionedModel.save({ transaction });
+
+		await transaction.commit();
+
+		return message.reply(`I sucessfully transferred **${amount}** <:coin:330926092703498240> to ${mentionedUser}!`);
 	}
 }
 
