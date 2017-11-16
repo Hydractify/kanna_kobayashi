13d8d69580bc82e59f5d9aeaddf25d154fca7f39
Pascal <wermail2002-kap@yahoo.de>
2017-11-16 22:06:33 +0100

    nitpicks(ModListCommand): a few consistency changes
    constructor:
    - Remove default command options
    - Make ModListCommand#statuses a Map to not have to map the first value of every entry to an array to check for a valid status
    run:
    - Simplify fetching of uncached members
    - Reply with
    ot a valid status because we use the negative form everywhere else as well
    - Remove unnecessary guild and guildMembers variable
    presenceList:
    - Check for status before checking perms since this is a cheaper operation
    general:
    - Rename guildMembers to members since it's clear that that's a members collection

--- commands/common/modlist.js
+++ commands/common/modlist.js
@@ -8,48 +8,38 @@ class ModListCommand extends Command {
 			aliases: ['mods'],
 			clientPermissions: ['EMBED_LINKS'],
 			coins: 0,
-			cooldown: 5000,
-			enabled: true,
 			description: 'See the mod list of the guild!',
 			examples: ['modlist online', 'modlist dnd'],
 			exp: 0,
 			name: 'modlist',
-			usage: 'modlist [status]',
-			permLevel: 0
+			usage: 'modlist [status]'
 		});
 
-		this.statuses = [
+		this.statuses = new Map(
 			['online', '<:online:339191830140944385>'],
 			['idle', '<:idle:339191829515993089>'],
 			['dnd', '<:dnd:339191829524381716>'],
 			['offline', '<:offline:339191829218066433>']
-		];
+		);
 	}
 
 	async run(message, [input], { authorModel }) {
-		const guild = message.guild.memberCount === message.guild.members.size
-			? message.guild
-			: await message.guild.fetchMembers();
-		const guildMembers = guild.members;
+		if (message.guild.memberCount !== message.guild.members.size) await message.guild.fetchMembers();
 
-		if (!input) return this.modList(message, guildMembers, authorModel);
+		if (!input) return this.modList(message, message.guild.members, authorModel);
 
-		const statuses = [];
-		for (const [status] of this.statuses) {
-			statuses.push(status);
-		}
+		input = input.toLowerCase();
+		if (this.status.has(input)) return this.presenceList(message, message.guild.members, input, authorModel);
 
-		if (statuses.includes(input)) return this.presenceList(message, guildMembers, input, authorModel);
-
-		return message.reply('that is an invalid status!');
+		return message.reply('that is not a valid status!');
 	}
 
-	presenceList(message, guildMembers, status, authorModel) {
+	presenceList(message, members, status, authorModel) {
 		let mods = new Set();
-		for (const member of guildMembers.values()) {
+		for (const member of members.values()) {
 			if (member.user.bot) continue;
-			if (!member.permissions.has(['KICK_MEMBERS', 'BAN_MEMBERS'])) continue;
 			if (member.presence.status !== status) continue;
+			if (!member.permissions.has(['KICK_MEMBERS', 'BAN_MEMBERS'])) continue;
 			mods.add(member.toString());
 		}
 		if (!mods.size) return message.reply(`there are no **${status}** mods!`);
@@ -62,7 +52,7 @@ class ModListCommand extends Command {
 		return message.channel.send(embed);
 	}
 
-	modList(message, guildMembers, authorModel) {
+	modList(message, members, authorModel) {
 		let mods = {
 			online: new Set(),
 			idle: new Set(),
@@ -70,7 +60,7 @@ class ModListCommand extends Command {
 			offline: new Set()
 		};
 
-		for (const member of guildMembers.values()) {
+		for (const member of members.values()) {
 			if (member.user.bot) continue;
 			if (!member.permissions.has(['KICK_MEMBERS', 'BAN_MEMBERS'])) continue;
 			mods[member.presence.status].add(member.toString());
