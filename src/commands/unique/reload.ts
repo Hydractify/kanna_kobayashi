d0ef9f2d4b6b294fbb81b3506ea825e39b370792
Pascal <wermail2002-kap@yahoo.de>
2017-12-20 22:04:18 +0100

    fix(reload): remove unused removed, reference new method in command handler

--- src/commands/unique/reload.ts
+++ src/commands/unique/reload.ts
@@ -25,30 +25,10 @@ class ReloadCommand extends Command {
 		return args;
 	}
 
-	/**
-	 * Reloads a command by name or alias
-	 * @param commandName Name or alias of the command to reload
-	 * @returns Shard ID and false for not found, true for success
-	 * and error for error
-	 */
-	public reload(commandName: string): [number, boolean | IPlainError] {
-		const command: Command = this.handler.resolveCommand(commandName.toLowerCase());
-
-		if (!command) return [this.client.shard.id, false];
-
-		try {
-			command.reload();
-		} catch (error) {
-			return [this.client.shard.id, Util.makePlainError(error)];
-		}
-
-		return [this.client.shard.id, true];
-	}
-
 	public async run(message: Message, [commandName]: string[]): Promise<Message | Message[]> {
 		if (!commandName) return message.reply('you should supply a command to reload.');
 
-		const results: string[] = await this.client.shard.broadcastEval(`this.commandHandler.reload('${commandName}')`)
+		const results: string[] = await this.client.shard.broadcastEval(`this.commandHandler.reloadCommand('${commandName}')`)
 			.then((result: [number, boolean | IPlainError][]) =>
 				result.map(
 					([id, other]: [number, boolean | IPlainError]) =>
