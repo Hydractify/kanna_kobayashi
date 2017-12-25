c87a1c55b0e2c89beb394b76a9fd501c516e5ee0
Pascal <wermail2002-kap@yahoo.de>
2017-12-25 11:33:12 +0100

    fix(Extensions): blockingly load extensions to avoid race conditions with discord.js

--- src/extensions/Extension.ts
+++ src/extensions/Extension.ts
@@ -1,16 +1,14 @@
 // tslint:disable:typedef only-arrow-functions ban-types interface-name
 
-import { readdir } from 'fs';
+import { readdirSync } from 'fs';
 import { basename, extname } from 'path';
-import { promisify } from 'util';
-
-const readdirAsync = promisify(readdir);
 
 import { Guild as GuildModel } from '../models/Guild';
 import { User as UserModel } from '../models/User';
 
 export async function extendAll(): Promise<void> {
-	const files: string[] = await readdirAsync(__dirname);
+	// This _HAS_ to be blocking duo race conditions with discord.js
+	const files: string[] = readdirSync(__dirname);
 	for (const file of files) {
 		if (extname(file) !== '.js') continue;
 		if (file === basename(__filename)) continue;
