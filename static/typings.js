705a127f4b68f3309816b66012d9dde21f2fe980
Pascal <wermail2002-kap@yahoo.de>
2018-03-03 12:26:46 +0100

    refactor(Scripts): rewrite typings script to js and move it to static

--- static/typings.js
+++ static/typings.js
@@ -1,11 +1,9 @@
-import { existsSync, writeFileSync } from 'fs';
-import { join } from 'path';
-import { get } from 'snekfetch';
-
-import { Logger } from '../structures/Logger';
+const { existsSync, writeFileSync } = require('fs');
+const { join } = require('path');
+const { get } = require('snekfetch');
 
 const sourceURL = 'https://raw.githubusercontent.com/discordjs/discord.js-typings/master/index.d.ts';
-const targetPath = join(__dirname, '..', '..', 'node_modules', 'discord.js', 'typings');
+const targetPath = join(__dirname,'..', 'node_modules', 'discord.js', 'typings');
 
 if (!existsSync(targetPath)) {
 	throw new Error(`${targetPath} does not exist!\nSure discord.js is already installed?`);
@@ -14,10 +12,10 @@ if (!existsSync(targetPath)) {
 get(sourceURL)
 	.then(({ text }) => writeFileSync(join(targetPath, 'index.d.ts'), text))
 	.then(() => {
-		Logger.instance.info('SUCESS', 'Updated typings!');
+		console.log('Updated typings');
 		process.exit(0);
 	})
 	.catch((error) => {
-		Logger.instance.error(error);
+		console.error('An error occured while updating the typings:', erro);
 		process.exit(1);
 	});
