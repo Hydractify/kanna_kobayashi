4dad9ae65f3343c95b5931362e850d95864a600d
Pascal <wermail2002-kap@yahoo.de>
2018-03-05 08:31:47 +0100

    refactor(static.typings): pipe resource into file rather than fully caching it before writing

--- static/typings.js
+++ static/typings.js
@@ -1,21 +1,20 @@
-const { existsSync, writeFileSync } = require('fs');
+const { existsSync, createWriteStream } = require('fs');
 const { join } = require('path');
 const { get } = require('snekfetch');
 
 const sourceURL = 'https://raw.githubusercontent.com/discordjs/discord.js-typings/master/index.d.ts';
-const targetPath = join(__dirname,'..', 'node_modules', 'discord.js', 'typings');
+const targetPath = join(__dirname, '..', 'node_modules', 'discord.js', 'typings');
 
 if (!existsSync(targetPath)) {
 	throw new Error(`${targetPath} does not exist!\nSure discord.js is already installed?`);
 }
 
 get(sourceURL)
-	.then(({ text }) => writeFileSync(join(targetPath, 'index.d.ts'), text))
-	.then(() => {
-		console.log('Updated typings');
-		process.exit(0);
-	})
-	.catch((error) => {
-		console.error('An error occured while updating the typings:', erro);
+	.on('error', (...error) => {
+		console.log('An error occured while updating the typings:', error);
 		process.exit(1);
+	})
+	.pipe(createWriteStream(join(targetPath, 'index.d.ts')))
+	.on('close', () => {
+		console.log('Updated typings');
 	});
