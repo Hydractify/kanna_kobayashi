e29da2e5ad2065be4569c92c38adb27efb7d40e1
SpaceEEC <wermail2002-kap@yahoo.de>
2018-04-19 17:37:09 +0200

    perf(CommandUsageCommand): replace raw query with sequelize Model#findAll

--- src/commands/common/commandUsage.ts
+++ src/commands/common/commandUsage.ts
@@ -1,7 +1,7 @@
 import { Message } from 'discord.js';
-import * as moment from 'moment';
-import { QueryTypes } from 'sequelize';
+import { col, fn, Op } from 'sequelize';
 
+import { CommandLog } from '../../models/CommandLog';
 import { Command } from '../../structures/Command';
 import { CommandHandler } from '../../structures/CommandHandler';
 import { MessageEmbed } from '../../structures/MessageEmbed';
@@ -22,95 +22,59 @@ class CommandUsageCommand extends Command {
 	}
 
 	public async run(message: Message, _: string[], { authorModel }: ICommandRunInfo): Promise<Message | Message[]> {
-		const entries: {
-			name: string;
-			total: number;
-			allTime: number;
-			recent: number;
-			lastHour: number;
-		}[] = await this.sequelize.query(`
-		SELECT
-			DISTINCT "command_logs"."command_name" as "name",
-			"total"."usages" AS "total",
-			"total"."all_time" AS "allTime",
-			"recent"."usages" AS "recent",
-			"recent"."last_hour" AS "lastHour"
-		FROM "command_logs"
-		LEFT JOIN (
-			SELECT
-				"command_logs"."command_name",
-				COUNT(*) AS "usages",
-				(SELECT count(*) FROM "command_logs") AS "all_time"
-			FROM "command_logs"
-			GROUP BY "command_logs"."command_name"
-			ORDER BY "usages" DESC
-			LIMIT 5
-		) "total" ON "total"."command_name" = "command_logs"."command_name"
-		LEFT JOIN (
-			SELECT
-				"command_logs"."command_name",
-				(SELECT count(*) FROM "command_logs" WHERE "command_logs"."run">=:lastHour) AS "last_hour",
-				COUNT(*) AS "usages"
-			FROM "command_logs"
-			WHERE "command_logs"."run">=:lastHour
-			GROUP BY "command_logs"."command_name"
-			ORDER BY "usages" DESC
-			LIMIT 5
-		) "recent" ON "recent"."command_name" = "command_logs"."command_name"
-		WHERE "total"."usages" NOTNULL OR "recent"."usages" NOTNULL
-		ORDER BY "total" DESC;`,
-			{
-				replacements: {
-					lastHour: moment().add(-1, 'hours').format('YYYY-MM-DD HH:mm:ss.SSS Z'),
-				},
-				type: QueryTypes.SELECT,
-			});
+		const [allTime, lastHour]: [CommandLog[], CommandLog[]] = await Promise.all([
+			CommandLog.findAll({
+				attributes: [[fn('COUNT', col('command_name')), 'count'], ['command_name', 'commandName']],
+				group: ['commandName'],
+				order: [['count', 'DESC']],
+			}),
+			CommandLog.findAll({
+				attributes: [[fn('COUNT', col('command_name')), 'count'], ['command_name', 'commandName']],
+				group: ['commandName'],
+				order: [['count', 'DESC']],
+				where: { run: { [Op.gt]: Date.now() - 35e5 } },
+			}),
+		]);
 
-		interface IEntry { count: number; name: string; }
 		let allTimeCount: number = 0;
-		let lastHourCount: number = 0;
-		const allTimeTop5: IEntry[] = [];
-		const lastHourTop5: IEntry[] = [];
-
-		for (const entry of entries) {
-			if (entry.allTime) allTimeCount = entry.allTime;
-			if (entry.lastHour) lastHourCount = entry.lastHour;
-			if (entry.recent) {
-				lastHourTop5.push({
-					count: Number(entry.recent),
-					name: titleCase(entry.name.replace(/([A-Z])/g, ' $1')),
-				});
-			}
-			if (entry.total) {
-				allTimeTop5.push({
-					count: Number(entry.total),
-					name: titleCase(entry.name.replace(/([A-Z])/g, ' $1')),
-				});
+		const allTimeTop5: string[] = [];
+		for (let { dataValues: { count, commandName } } of allTime as any) {
+			count = Number(count);
+			allTimeCount += count;
+			if (allTimeTop5.length <= 5) {
+				allTimeTop5.push(
+					`${allTimeTop5.length + 1}. ${titleCase(commandName.replace(/([A-Z])/g, ' $1'))}: ${count.toLocaleString()}`,
+				);
 			}
 		}
-		lastHourTop5.sort((a: IEntry, b: IEntry) => b.count - a.count);
 
-		const allTimeMapped: string[] = allTimeTop5
-			.map((entry: IEntry, i: number) => `${i + 1}. ${entry.name}: ${entry.count.toLocaleString()}`);
+		let lastHourCount: number = 0;
+		const lastHourTop5: string[] = [];
+		for (let { dataValues: { count, commandName } } of lastHour as any) {
+			count = Number(count);
+			lastHourCount += count;
+			if (lastHourTop5.length <= 5) {
+				lastHourTop5.push(
+					`${lastHourTop5.length + 1}. ${titleCase(commandName.replace(/([A-Z])/g, ' $1'))}: ${count.toLocaleString()}`,
+				);
+			}
+		}
 
-		allTimeMapped.unshift(
-			`Count: ${Number(allTimeCount).toLocaleString()}`,
+		allTimeTop5.unshift(
+			`Count: ${allTimeCount.toLocaleString()}`,
 			'\u200b',
 		);
 
-		const lastHourMapped: string[] = lastHourTop5
-			.map((entry: IEntry, i: number) => `${i + 1}. ${entry.name}: ${entry.count.toLocaleString()}`);
-
-		lastHourMapped.unshift(
-			`Count: ${Number(lastHourCount).toLocaleString()}`,
+		lastHourTop5.unshift(
+			`Count: ${lastHourCount.toLocaleString()}`,
 			'\u200b',
 		);
 
 		const embed: MessageEmbed = MessageEmbed.common(message, authorModel)
 			.setThumbnail(message.guild.iconURL())
 			.setTitle('Command Usage Statistics')
-			.addField('All Time', allTimeMapped, true)
-			.addField('Last Hour', lastHourMapped, true);
+			.addField('All Time', allTimeTop5, true)
+			.addField('Last Hour', lastHourTop5, true);
 
 		return message.channel.send(embed);
 	}
