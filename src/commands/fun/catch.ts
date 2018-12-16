dc846184b49036098c1319a8adafdcbcb9fd1f44
Alexandre Cavalheiro S. Tiago Da Silva <alexandre.cssilva@gmail.com>
2018-12-15 22:04:10 -0200

    [~] CatchCommand -> Make the command 'for fun.'

--- src/commands/fun/catch.ts
+++ src/commands/fun/catch.ts
@@ -4,7 +4,6 @@ import { Transaction } from 'sequelize';
 import { Item } from '../../models/Item';
 import { Command } from '../../structures/Command';
 import { CommandHandler } from '../../structures/CommandHandler';
-import { Emojis } from '../../types/Emojis';
 import { ICommandRunInfo } from '../../types/ICommandRunInfo';
 import { Items } from '../../types/Items';
 
@@ -19,45 +18,29 @@ class CatchCommand extends Command {
 		});
 	}
 
-	public async parseArgs(message: Message, args: string[], { authorModel }: ICommandRunInfo): Promise<string | Item[]> {
-		const [net]: Item[] = await authorModel.$get<Item>('items', { where: { name: Items.BUG_NET } }) as Item[];
-
-		if (!net || !net.getCount()) return 'You must have at least one Bug Net to use this command!';
-		return [net];
-	}
-
 	public async run(message: Message, item: Item[], { authorModel }: ICommandRunInfo): Promise<Message | Message[]> {
 		let bugAmount: number = 0;
-		let netBreak: boolean = false;
 
-		const breakChance: number = Math.floor(Math.random() * 1000);
 		const bugChance: number = Math.floor(Math.random() * 100) + 1;
 
 		if (bugChance === 100) {
 			bugAmount = 6;
-			netBreak = breakChance > 445;
 		} else if (bugChance > 90) {
 			bugAmount = 5;
-			netBreak = breakChance > 500;
 		} else if (bugChance > 80) {
 			bugAmount = 4;
-			netBreak = breakChance > 550;
 		} else if (bugChance > 70) {
 			bugAmount = 3;
-			netBreak = breakChance > 635;
 		} else if (bugChance > 60) {
 			bugAmount = 2;
-			netBreak = breakChance > 700;
 		} else if (bugChance > 50) {
 			bugAmount = 1;
-			netBreak = breakChance > 850;
 		}
 
-		if (netBreak || bugAmount) {
+		if (bugAmount) {
 			const transaction: Transaction = await this.sequelize.transaction();
 			try {
-				await Promise.all<number | null, number | null>([
-					netBreak ? authorModel.addItem(Items.BUG_NET, -1, { transaction }) : null,
+				await Promise.all<number | null>([
 					bugAmount ? authorModel.addItem(Items.BUG, bugAmount, { transaction }) : null,
 				]);
 
@@ -69,10 +52,7 @@ class CatchCommand extends Command {
 			}
 		}
 
-		return message.channel.send([
-			`You ${bugAmount ? `got **${bugAmount}** bugs!` : 'did not get any bugs...'}`,
-			`And your Bug Net ${netBreak ? 'broke...' : 'did not break!'} ${Emojis.KannaRun}`,
-		].join(' '));
+		return message.channel.send(`You ${bugAmount ? `got **${bugAmount}** bugs! 🐛` : 'did not get any bugs...'}`);
 	}
 }
 
