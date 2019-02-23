ce5761e70873fb677eb77f72f20fa03fc57fc5d2
SpaceEEC <spaceeec@yahoo.com>
2019-02-23 18:52:06 +0100

    fix(BlockListCommand): inform the user when sending a dm failed

--- src/commands/user/blocklist.ts
+++ src/commands/user/blocklist.ts
@@ -1,9 +1,12 @@
-import { Message, User } from 'discord.js';
+import { Constants, DiscordAPIError, Message, User } from 'discord.js';
 
 import { User as UserModel } from '../../models/User';
 import { Command } from '../../structures/Command';
 import { CommandHandler } from '../../structures/CommandHandler';
+import { Emojis } from '../../types/Emojis';
 import { ICommandRunInfo } from '../../types/ICommandRunInfo';
+
+const { APIErrors } = Constants;
 
 class BlockListCommand extends Command {
 	constructor(handler: CommandHandler) {
@@ -18,31 +21,45 @@ class BlockListCommand extends Command {
 	public async run(message: Message, args: string[], { authorModel }: ICommandRunInfo): Promise<Message | Message[]> {
 		const blocked: UserModel[] = await authorModel.$get('blocked') as UserModel[];
 
-		if (!blocked.length) {
-			await message.author.send('You did not block anybody.');
-		} else {
-			const tags: string[] = await Promise
-				.all(blocked.map(
-					(block: UserModel) => this.client.users.get(block.id) || this.client.users.fetch(block.id)),
-				).then(
-					(users: User[]) => users.map((user: User) => user.tag),
-				);
+		try {
+			if (!blocked.length) {
+				await message.author.send('You did not block anybody.');
+			} else {
+				const tags: string[] = await Promise
+					.all(blocked.map(
+						(block: UserModel) => this.client.users.get(block.id) || this.client.users.fetch(block.id)),
+					).then(
+						(users: User[]) => users.map((user: User) => user.tag),
+					);
 
-			let out: string = '';
-			for (const tag of tags) {
-				if (tag.length + tags.length > 1900) {
-					out += '...';
-					break;
+				let out: string = '';
+				for (const tag of tags) {
+					if (tag.length + tags.length > 1900) {
+						out += '...';
+						break;
+					}
+
+					out += `, **${tag}**`;
 				}
+				out = out.slice(2);
 
-				out += `, **${tag}**`;
+				await message.author.send(`The users you blocked from using interactive commands on you:\n${out}`);
 			}
-			out = out.slice(2);
 
-			await message.author.send(`The users you blocked from using interactive commands on you:\n${out}`);
+			return message.reply('sent you the requested info.');
+		} catch (e) {
+			if ((e as DiscordAPIError).code === APIErrors.CANNOT_MESSAGE_USER) {
+				return message.reply([
+					'I have failed to send a DM to you!',
+					'Check if you have disabled your DMs or',
+					'if you have me blocked.',
+					Emojis.KannaShy,
+				].join(' '));
+			}
+
+			return Promise.reject(e);
 		}
 
-		return message.reply('sent you the requested info.');
 	}
 }
 
